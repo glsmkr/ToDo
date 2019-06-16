d712b56da49a0cefb997b9e6ef9b382707648a1d
Julian A. Fordyce <jfordyce@Julians-MacBook-Pro.local>
2019-06-16 13:18:15 -0400

    app now works

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -9,11 +9,39 @@
 import SwiftUI
 
 struct ContentView : View {
+    @State var todo = ""
+    @State var todos: [String] = []
+    
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                VStack {
+                    VStack {
+                        Group {
+                            TextField($todo, placeholder: Text("Enter a todo: ")).padding(12)
+                            }.background(Color.white).clipShape(RoundedRectangle(cornerRadius: 5))
+                            .shadow(radius: 4)
+                            .padding(8)
+                        
+                        }
+                        
+                    }
+                
+                List (todos.identified(by: \.self)) {
+                    Text($0)
+                }
+            }.navigationBarItems(trailing: Button(action: {
+                if !self.todo.isEmpty {
+                self.todos.append(self.todo)
+                }
+                self.todo = ""
+            }, label: {
+                Text("Save").color(.blue)
+            }))
+        }
     }
 }
-
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
